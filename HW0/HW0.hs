7ba97bab5908f23d79db00883ddfcca942625b3c
Second User <seconduser@Tamirs-MacBook-Pro.local>
2023-04-12 19:58:02 +0300

    until lcm

--- HW0/HW0.hs
+++ HW0/HW0.hs
@@ -4,11 +4,13 @@
 -- Tells HLS to show warnings, and the file won't be compiled if there are any warnings, e.g.,
 -- eval (-- >>>) won't work.
 {-# OPTIONS_GHC -Wall -Werror #-}
+{-# OPTIONS_GHC -Wno-unused-imports #-}
 
 module HW0 where
 
 -- These import statement ensures you aren't using any "advanced" functions and types, e.g., lists.
-import Prelude (Bool (..), Eq (..), Int, Integer, Num (..), Ord (..), div, error, flip, id, mod, ($), (&&), (.), (||))
+-- These import statement ensures you aren't using any "advanced" functions and types, e.g., lists.
+import Prelude (Bool (..), Eq (..), Int, Integer, Num (..), Ord (..), div, error, flip, id, mod, ($), (&&), (.), (||), RealFrac (ceiling))
 
 -- For a bit of an extra challange, use the below import list instead, and implement the missing functions yourselves!
 -- import Prelude (Num(..), Bool(..), Int, Integer, Eq(..), Ord(..), div, error)
@@ -17,26 +19,112 @@ import Prelude (Bool (..), Eq (..), Int, Integer, Num (..), Ord (..), div, error
 -- Higher order functions. Most of these should be self-explanatory.
 -- ************************************************************** --
 
+-----------helper functions----------------
+plus1 :: Int -> Int
+plus1 x = x + 1 
+
+multiBy2 :: Int -> Int
+multiBy2 x = x * 2 
+
+plus3 :: (Int, Int, Int) -> Int 
+plus3 (a, b, c) = a + b + c 
+
+uncurryplus3:: Int -> Int -> Int -> Int
+uncurryplus3 a b c = a + b + c
+
+uncurryminus3:: Int -> Int -> Int -> Int
+uncurryminus3 a b c = a - b - c
+
+smallerThen10 :: Int -> Bool
+smallerThen10 x = x < 10
+
+biggerThen20 :: Int -> Bool
+biggerThen20 x = x > 20
+
+-----------implementations----------------
+
 const :: a -> b -> a
+const a _  = a 
+
 (.>) :: (a -> b) -> (b -> c) -> a -> c
+(.>) f g a = g (f a)  
+
 curry3 :: ((a, b, c) -> d) -> a -> b -> c -> d
+curry3 f a b c = f (a, b, c)
+
 uncurry3 :: (a -> b -> c -> d) -> (a, b, c) -> d
+uncurry3 f (a, b, c) = f a b c
+
 rotate :: (a -> b -> c -> d) -> c -> a -> b -> d
+rotate f c a b = f a b c 
+
 lotate :: (a -> b -> c -> d) -> b -> c -> a -> d
+lotate f b c a = f a b c 
 
 -- or (> 0) (< 0) 5 returns True
 -- or (> 0) (< 0) 0 returns False
 or :: (a -> Bool) -> (a -> Bool) -> a -> Bool
+or f g a = f a || g a
+
 -- and (> 0) (< 0) 5 returns False
 -- and (> 0) (> 10) 20 returns True
 and :: (a -> Bool) -> (a -> Bool) -> a -> Bool
--- Did you implement the above two without duplication?
+and f g a = f a && g a
 
 -- Applies the function until some condition holds.
 -- until (+1) (> 0) 5 returns 5
 -- until (*2) (> 10) 1 returns 16
 -- until (*1) (> 0) 0 would never terminate!
 until :: (a -> a) -> (a -> Bool) -> a -> a
+until f g a = if g a then a else until f g (f a) 
+
+-- ******************************** --
+-- Numerical functions and algorithms.
+-- ******************************** --
+
+-- Returns the greatest common divisor (GCD) of two numbers. The GCD is always a positive number.
+-- gcd 123456 432 is 48.
+gcd :: Int -> Int -> Int
+gcd a 0 = abs a
+gcd a b = gcd b (a `mod` b)
+    
+-----------evaluate----------------
+{-
+>>> const 2 5 
+2
+
+>>> (.>) plus1 plus1 3
+5
+
+>>> curry3 plus3 1 2 4 
+7
+
+>>> uncurry3 uncurryplus3 (3,3,4)
+10
+
+>>> rotate uncurryminus3 2 3 4 
+-3
+
+>>> lotate uncurryminus3 2 3 4
+-1
+
+>>> or smallerThen10 biggerThen20 22
+True
+
+>>> and smallerThen10 biggerThen20 22
+False
+
+>>> until multiBy2 biggerThen20 3
+24
+
+>>> gcd 24 34
+2
+
+-}
+
+{- 
+
+-- Did you implement the above two without duplication?
 
 -- ******************************** --
 -- Numerical functions and algorithms.
@@ -95,3 +183,4 @@ nthTwinPrime :: Int -> Int
 -- Returns true if the number is a perfect number. A perfect number is equal to the sum of its
 -- divisors. So 6 (1 + 2 + 3) a is a perfect number, so is 28 (1 + 2 + 4 + 7 + 14), and so is 496.
 isPerfectNumber :: Int -> Bool
+-}
